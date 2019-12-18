cbbc5ffab85d4a4287a901a4e7e9454c8c6b23fe
Andre Goh <andregoh1996@gmail.com>
2019-12-18 18:10:56 +0800

    update 18/12

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customers from './components/customers';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
+        <div className="nav">
+          <img></img>
+          <nav className="links">
+            <a>About</a>
+            <a>Gallery</a>
+            <a>Services</a>
+            <a>Contact</a>
+          </nav>
+        </div>
+        <div className="hero section">
+          <h2>sinhockheng</h2>
+          <h1>upholstry</h1>
+          <p>
+            we stand by integrity and dedication to provide the finest
+            craftsmanship to our customers.
+          </p>
+        </div>
+        <div className="about section">
+          <img src={require("./images/about.jpg")}></img>
+          <p>
+            Established in 1980, SSH has a vast experience in upholstery for
+            automotives. Over the years, we have added windscreen replacement
+            and insurance claims into our services as well. <br></br>
+            <br></br>Our track record over the years have proven our finest
+            craftsmanship and precision for automotive upholstery.We have done
+            projects ranging from classic vintage cars to luxury continental
+            sports cars, so rest assured when you leave upholstery project with
+            us.
+          </p>
+        </div>
+        <div className="services section">
+          <h1>OUR SERVICES</h1>
+          <div className="service-icons">
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+            <div className="serv-block">
+              <img></img>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div className="insurance section"></div>
+        <div className="contact section"></div>
       </div>
     );
   }
