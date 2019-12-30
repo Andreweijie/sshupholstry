f2143ae2835366a1189b6b7543005ddd97401c7f
Andre Goh <andregoh1996@gmail.com>
2019-12-30 21:05:19 +0800

    update 30/12

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import Images from "./components/Images";
+import { Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route exact path="/gallery" component={Images}></Route>
         <div className="nav">
           <img></img>
           <nav className="links">
@@ -18,66 +20,122 @@ class App extends Component {
           <h2>sinhockheng</h2>
           <h1>upholstry</h1>
           <p>
-            we stand by integrity and dedication to provide the finest
+            We stand by integrity and dedication to provide the finest
             craftsmanship to our customers.
           </p>
         </div>
-        <div className="about section">
+        <div id="about" className="about section">
           <img src={require("./images/about.jpg")}></img>
-          <p>
-            Established in 1980, SSH has a vast experience in upholstery for
-            automotives. Over the years, we have added windscreen replacement
-            and insurance claims into our services as well. <br></br>
-            <br></br>Our track record over the years have proven our finest
-            craftsmanship and precision for automotive upholstery.We have done
-            projects ranging from classic vintage cars to luxury continental
-            sports cars, so rest assured when you leave upholstery project with
-            us.
-          </p>
+          <div className="about-text">
+            <h1>About SSH Upholstery</h1>
+            <p>
+              Established in 1980, SSH has a vast experience in upholstery for
+              automotives. Over the years, we have added windscreen replacement
+              and insurance claims into our services as well. <br></br>
+              <br></br>Our track record over the years have proven our finest
+              craftsmanship and precision for automotive upholstery.We have done
+              projects ranging from classic vintage cars to luxury continental
+              sports cars, so rest assured when you leave upholstery project
+              with us.
+            </p>
+          </div>
         </div>
-        <div className="services section">
+        <div id="services" className="services section">
           <h1>OUR SERVICES</h1>
           <div className="service-icons">
             <div className="serv-block">
-              <img></img>
-              <span></span>
+              <img src="https://i.imgur.com/YLKeiSF.png"></img>
+              <span>steering wheel</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/hwIPIgF.png"></img>
+              <span>carpet</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/QYEZrKA.png"></img>
+              <span>door panel</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/CDn66Jr.png"></img>
+              <span>convertible top</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/gqWtkqv.png"></img>
+              <span>roof-lining</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/1mUnZWH.png"></img>
+              <span>car seats</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/H20zWiA.png"></img>
+              <span>dashboard</span>
+            </div>
+            <div className="serv-block">
+              <img src="https://i.imgur.com/UXadTYJ.png"></img>
+              <span>gear knob</span>
             </div>
             <div className="serv-block">
               <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
-            </div>
-            <div className="serv-block">
-              <img></img>
-              <span></span>
+              <span>furniture</span>
             </div>
           </div>
         </div>
-        <div className="insurance section"></div>
-        <div className="contact section"></div>
+        <div className="insurance section">
+          <h1>windscreen insurance</h1>
+          <h3>windscreen insurance claims procedure</h3>
+          <div className="ins-block">
+            <img src="https://i.imgur.com/vYbdeIo.png"></img>
+            <span>1. Come to our workshop with your insurance certificate</span>
+          </div>
+          <div className="ins-block">
+            <img
+              style={{ paddingLeft: 20, height: 160 }}
+              src="https://i.imgur.com/mqeeKiJ.png"
+            ></img>
+            <span>2. We will submit your claims for approval</span>
+          </div>
+          <div className="ins-block">
+            <img
+              style={{ height: 100 }}
+              src="https://i.imgur.com/KWrPPsG.png"
+            ></img>
+            <span>
+              3. Once it is approved, we will commence work to remove the
+              damaged windscreen
+            </span>
+          </div>
+          <div className="ins-block">
+            <img src="https://i.imgur.com/YLKeiSF.png"></img>
+            <span>
+              4. After 3-4 hours, your vehicle is ready for collection!
+            </span>
+          </div>
+        </div>
+        <div className="partners section"></div>
+        <div id="contact" className="contact section">
+          <div className="contact-form">
+            <h1>CONTACT US</h1>
+            <form>
+              <div className="form-group">
+                <label>Name</label>
+                <input type="text"></input>
+              </div>
+              <div className="form-group">
+                <label>Email</label>
+                <input type="email"></input>
+              </div>
+              <div className="form-group">
+                <label>Message</label>
+                <textarea></textarea>
+              </div>
+            </form>
+          </div>
+          <div className="contact-info"></div>
+        </div>
+        <footer>
+          designed by andreweijie|<a>privacy policy</a>
+        </footer>
       </div>
     );
   }
---
+++ client/src/components/Images.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Gallery from "react-photo-gallery";
+
+export default class Images extends Component {
+  render() {
+    return <div></div>;
+  }
+}
