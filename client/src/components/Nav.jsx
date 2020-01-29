45c598a3b10d85d9ac41bc43313f7e40f5d5bd5a
Andre Goh <andregoh1996@gmail.com>
2020-01-29 23:14:37 +0800

    update 29/1-2

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,6 +6,7 @@ export default class Nav extends Component {
     return (
       <div className="nav">
         <nav className="links">
+          <img className="nav-logo" src={require("../images/logo.svg")}></img>
           <Link to="/gallery">Gallery</Link>
           <a>Services</a>
           <Link to="/contact">Contact</Link>
