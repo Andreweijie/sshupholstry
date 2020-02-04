b189139c1f2c9dafc57861c1fe5d88db526042c0
Andre Goh <andregoh1996@gmail.com>
2020-02-04 16:54:43 +0800

    update 4/2

--- client/src/App.js
+++ client/src/App.js
@@ -75,7 +75,25 @@ class App extends Component {
           </div>
         </div>
         <div id="services" className="services section">
-          <div className="services-list"></div>
+          <div className="services-list">
+            <h1>OUR SERVICES INCLUDE</h1>
+            <ul>
+              <li>ARM REST</li>
+              <li>CARPET</li>
+              <li>DOOR PANEL</li>
+              <li>DASHBOARD</li>
+              <li>FURNITURE</li>
+              <li>GEAR KNOB</li>
+              <li>HANDBRAKE</li>
+              <li>ROOF-LINING</li>
+              <li>STEERING WHEEL</li>
+              <li>SUN VISOR</li>
+              <li>SOFT CONVERTIBLE TOP</li>
+              <li>SEATS</li>
+              <li>VAN INTERIOR</li>
+              <li>WINDSCREEN & CLAIMS</li>
+            </ul>
+          </div>
           <div className="service-icons">
             <div className="serv-block">
               <img src={require("./images/svg/car-roof.svg")}></img>
@@ -87,7 +105,7 @@ class App extends Component {
               <img src={require("./images/svg/car-seat.svg")}></img>
             </div>
             <div className="serv-block">
-              <img src={require("./images/svg/car-top.svg")}></img>
+              <img src={require("./images/svg/car-knob.svg")}></img>
             </div>
             <div className="serv-block">
               <img src={require("./images/svg/car-wheel.svg")}></img>
@@ -99,151 +117,14 @@ class App extends Component {
               <img src={require("./images/svg/car-dash.svg")}></img>
             </div>
             <div className="serv-block">
-              <img src={require("./images/svg/car-pillar.svg")}></img>
-            </div>
-            <div className="serv-block">
-              <img src={require("./images/svg/car-furniture.svg")}></img>
-            </div>
-            <div className="serv-block">
               <img src={require("./images/svg/car-visor.svg")}></img>
             </div>
             <div className="serv-block">
-              <img src={require("./images/svg/car-knob.svg")}></img>
+              <img src={require("./images/svg/car-top.svg")}></img>
             </div>
-            <div className="serv-block">
-              <img src={require("./images/svg/car-van.svg")}></img>
-            </div>
-          </div>
-          <div className="booking">
-            <h4>
-              Pick up and delivery Skip the hassle! Let us pick up your vehicle
-              at home and return it to you when it is completed. <br></br>Book
-              an appointment for a Pick Up & Delivery service @ S$50 round trip
-            </h4>
-            <form>
-              <div className="form-group">
-                <label>Name*</label>
-                <input
-                  required
-                  value={this.state.name}
-                  onChange={this.onChange}
-                  id="name"
-                  type="text"
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Mobile No*</label>
-                <input
-                  required
-                  value={this.state.mobile}
-                  onChange={this.onChange}
-                  id="mobile"
-                  type="text"
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Email*</label>
-                <input
-                  required
-                  value={this.state.email}
-                  onChange={this.onChange}
-                  id="email"
-                  type="Email"
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Vehicle No*</label>
-                <input
-                  required
-                  value={this.state.VehicleNo}
-                  onChange={this.onChange}
-                  id="VehicleNo"
-                  type="text"
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Vehicle Model*</label>
-                <input
-                  required
-                  value={this.state.vehicleModel}
-                  onChange={this.onChange}
-                  id="vehicleModel"
-                  type="text"
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Date*</label>
-                <input
-                  required
-                  value={this.state.date}
-                  onChange={this.onChange}
-                  type="date"
-                  id="date"
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Time*</label>
-                <select
-                  required
-                  value={this.state.time}
-                  onChange={this.onChange}
-                  id="time"
-                >
-                  <option value="9am - 12pm">9am - 12pm</option>
-                  <option value="1pm - 3pm">1pm - 3pm</option>
-                  <option value="4pm - 6pm">4pm - 6pm</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <label>Service*</label>
-                <select
-                  required
-                  value={this.state.service}
-                  onChange={this.onChange}
-                  id="service"
-                >
-                  <option value="Arm Rest">Arm Rest</option>
-                  <option value="Carpet">Carpet</option>
-                  <option value="Door Panel">Door Panel</option>
-                  <option value="Dashboard">Dashboard</option>
-                  <option value="Furniture">Furniture</option>
-                  <option value="Gear Knob">Gear Knob</option>
-                  <option value="Handbrake">Handbrake</option>
-                  <option value="Roof-Lining">Roof-Lining</option>
-                  <option value="Steering Wheel">Steering Wheel</option>
-                  <option value="Sun Visor">Sun Visor</option>
-                  <option value="Soft Convertible Top">
-                    Soft Convertible Top
-                  </option>
-                  <option value="Seats">Seats</option>
-                  <option value="Van Interior Conversion">
-                    Van Interior Conversion
-                  </option>
-                  <option value="Insurance Claim">Insurance Claim</option>
-                  <option value="Van Window Conversion">
-                    Van Window Conversion
-                  </option>
-                  <option value="V-Kool">V-Kool</option>
-                </select>
-              </div>
-              <div className="form-group full-width">
-                <label>Remarks</label>
-                <textarea
-                  value={this.state.remarks}
-                  onChange={this.onChange}
-                  id="remarks"
-                ></textarea>
-              </div>
-            </form>
-            <button
-              className="full-width"
-              type="submit"
-              onClick={this.onSubmit}
-            >
-              {this.state.loading}
-            </button>
           </div>
         </div>
+        <div className="booking section"></div>
 
         <div className="partners section">
           <h1 style={{ textAlign: "center" }}>Insurance Partners</h1>
