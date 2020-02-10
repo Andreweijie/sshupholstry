35276f55a4a6c5c0ef206a79f7be666b86fa72a4
Andre Goh <andregoh1996@gmail.com>
2020-02-10 10:07:51 +0800

    update 9/2

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Nav from "./components/Nav";
 library.add(faFacebookSquare, faInstagram, faWhatsapp);
 class App extends Component {
   state = {
+    appt: false,
     name: "",
     email: "",
     mobile: "",
@@ -39,6 +40,9 @@ class App extends Component {
       "09:00 - 18:00",
       "09:00 - 17:00"
     ]
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.checked });
   };
   render() {
     return (
@@ -146,7 +150,11 @@ class App extends Component {
           <div className="form-box">
             <div className="form-switch">
               <span>appointment</span>
-              <Switch></Switch>
+              <Switch
+                name="appt"
+                onChange={this.handleChange}
+                checked={this.state.appt}
+              />
               <span>pickup/delivery</span>
             </div>
 
@@ -181,89 +189,102 @@ class App extends Component {
                   type="Email"
                 ></input>
               </div>
-              <div className="form-group first-col">
-                <label>Vehicle No*</label>
-                <input
-                  required
-                  value={this.state.VehicleNo}
-                  onChange={this.onChange}
-                  id="VehicleNo"
-                  type="text"
-                ></input>
-              </div>
-              <div className="form-group first-col">
-                <label>Vehicle Model*</label>
-                <input
-                  required
-                  value={this.state.vehicleModel}
-                  onChange={this.onChange}
-                  id="vehicleModel"
-                  type="text"
-                ></input>
-              </div>
-              <div className="form-group second-col">
-                <label>Service*</label>
-                <select
-                  required
-                  value={this.state.service}
-                  onChange={this.onChange}
-                  id="service"
-                >
-                  <option value="Arm Rest">Arm Rest</option>
-                  <option value="Carpet">Carpet</option>
-                  <option value="Door Panel">Door Panel</option>
-                  <option value="Dashboard">Dashboard</option>
-                  <option value="Furniture">Furniture</option>
-                  <option value="Gear Knob">Gear Knob</option>
-                  <option value="Handbrake">Handbrake</option>
-                  <option value="Roof-Lining">Roof-Lining</option>
-                  <option value="Steering Wheel">Steering Wheel</option>
-                  <option value="Sun Visor">Sun Visor</option>
-                  <option value="Soft Convertible Top">
-                    Soft Convertible Top
-                  </option>
-                  <option value="Seats">Seats</option>
-                  <option value="Van Interior Conversion">
-                    Van Interior Conversion
-                  </option>
-                  <option value="Insurance Claim">Insurance Claim</option>
-                  <option value="Van Window Conversion">
-                    Van Window Conversion
-                  </option>
-                  <option value="V-Kool">V-Kool</option>
-                </select>
-              </div>
-              <div className="form-group second-col">
-                <label>Date*</label>
-                <input
-                  required
-                  value={this.state.date}
-                  onChange={this.onChange}
-                  type="date"
-                  id="date"
-                ></input>
-              </div>
-              <div className="form-group second-col">
-                <label>Time*</label>
-                <select
-                  required
-                  value={this.state.time}
-                  onChange={this.onChange}
-                  id="time"
-                >
-                  <option value="9am - 12pm">9am - 12pm</option>
-                  <option value="1pm - 3pm">1pm - 3pm</option>
-                  <option value="4pm - 6pm">4pm - 6pm</option>
-                </select>
-              </div>
-              <div className="form-group full-width">
-                <label>Remarks</label>
-                <textarea
-                  value={this.state.remarks}
-                  onChange={this.onChange}
-                  id="remarks"
-                ></textarea>
-              </div>
+              {!this.state.appt ? (
+                <div className="form-group first-col">
+                  <label>Vehicle No*</label>
+                  <input
+                    required
+                    value={this.state.VehicleNo}
+                    onChange={this.onChange}
+                    id="VehicleNo"
+                    type="text"
+                  ></input>
+                </div>
+              ) : null}
+              {!this.state.appt ? (
+                <div className="form-group first-col">
+                  <label>Vehicle Model*</label>
+                  <input
+                    required
+                    value={this.state.vehicleModel}
+                    onChange={this.onChange}
+                    id="vehicleModel"
+                    type="text"
+                  ></input>
+                </div>
+              ) : null}
+              {!this.state.appt ? (
+                <div className="form-group second-col">
+                  <label>Service*</label>
+                  <select
+                    required
+                    value={this.state.service}
+                    onChange={this.onChange}
+                    id="service"
+                  >
+                    <option value="Arm Rest">Arm Rest</option>
+                    <option value="Carpet">Carpet</option>
+                    <option value="Door Panel">Door Panel</option>
+                    <option value="Dashboard">Dashboard</option>
+                    <option value="Furniture">Furniture</option>
+                    <option value="Gear Knob">Gear Knob</option>
+                    <option value="Handbrake">Handbrake</option>
+                    <option value="Roof-Lining">Roof-Lining</option>
+                    <option value="Steering Wheel">Steering Wheel</option>
+                    <option value="Sun Visor">Sun Visor</option>
+                    <option value="Soft Convertible Top">
+                      Soft Convertible Top
+                    </option>
+                    <option value="Seats">Seats</option>
+                    <option value="Van Interior Conversion">
+                      Van Interior Conversion
+                    </option>
+                    <option value="Insurance Claim">Insurance Claim</option>
+                    <option value="Van Window Conversion">
+                      Van Window Conversion
+                    </option>
+                    <option value="V-Kool">V-Kool</option>
+                  </select>
+                </div>
+              ) : null}
+              {!this.state.appt ? (
+                <div className="form-group second-col">
+                  <label>Date*</label>
+                  <input
+                    required
+                    value={this.state.date}
+                    onChange={this.onChange}
+                    type="date"
+                    id="date"
+                  ></input>
+                </div>
+              ) : null}
+              {!this.state.appt ? (
+                <div className="form-group second-col">
+                  <label>Time*</label>
+                  <select
+                    required
+                    value={this.state.time}
+                    onChange={this.onChange}
+                    id="time"
+                  >
+                    <option value="9am - 12pm">9am - 12pm</option>
+                    <option value="1pm - 3pm">1pm - 3pm</option>
+                    <option value="4pm - 6pm">4pm - 6pm</option>
+                  </select>
+                </div>
+              ) : null}
+              {!this.state.appt ? (
+                <div className="form-group full-width">
+                  <label>Remarks</label>
+                  <textarea
+                    value={this.state.remarks}
+                    onChange={this.onChange}
+                    id="remarks"
+                  ></textarea>
+                </div>
+              ) : null}
+
               <button className="full-width">Submit</button>
             </form>
           </div>
@@ -313,13 +334,18 @@ class App extends Component {
             </div>
             <div className="info-block">
               <h1>EMAIL</h1>
-              <h2>info@shhupholstery.com</h2>
+              <a
+                href="mailto:info@shhupholstery.com?Subject=Enquiry"
+                target="_top"
+              >
+                info@shhupholstery.com
+              </a>
             </div>
             <div className="info-block">
               <h1>LOCATION</h1>
-              <h2>
+              <a href="https://www.google.com.sg/maps/place/Sin+Hock+Heng+Upholstery/@1.3733373,103.856409,15z/data=!4m5!3m4!1s0x31da1724089c4d9b:0x3fd503e1536b2410!8m2!3d1.3586024!4d103.8388447">
                 7 Sin Ming Industrial Estate Sector C, #01-90, Singapore 575642
-              </h2>
+              </a>
             </div>
             <div className="info-block">
               <h1>OPENING HOURS</h1>
@@ -351,12 +377,18 @@ class App extends Component {
         </div>
         <div className="footer">
           <div className="social">
-            <FontAwesomeIcon size="2x" icon={faWhatsapp}></FontAwesomeIcon>
-            <FontAwesomeIcon size="2x" icon={faInstagram}></FontAwesomeIcon>
-            <FontAwesomeIcon
-              size="2x"
-              icon={faFacebookSquare}
-            ></FontAwesomeIcon>
+            <a href="https://wa.me/6596373996">
+              <FontAwesomeIcon size="2x" icon={faWhatsapp}></FontAwesomeIcon>
+            </a>
+            <a href="https://www.instagram.com/shh.upholstery/">
+              <FontAwesomeIcon size="2x" icon={faInstagram}></FontAwesomeIcon>
+            </a>
+            <a href="https://www.facebook.com/sshupholstery/">
+              <FontAwesomeIcon
+                size="2x"
+                icon={faFacebookSquare}
+              ></FontAwesomeIcon>
+            </a>
           </div>
           privacy policy
         </div>
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 export default class Nav extends Component {
   render() {
@@ -7,10 +6,9 @@ export default class Nav extends Component {
       <div className="nav">
         <nav className="links">
           <img className="nav-logo" src={require("../images/logo.svg")}></img>
-          <Link to="/gallery">Gallery</Link>
           <a>Services</a>
-          <Link to="/contact">Contact</Link>
-          <a class="book">Book Now</a>
+          <a>Contact</a>
+          <a>Book Now</a>
         </nav>
       </div>
     );
